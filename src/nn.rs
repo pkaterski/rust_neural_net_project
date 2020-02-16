cb4df05d64163ad2cef6044eb7294422ff20d93e
Parashkev Katerski <pkaterski@gmail.com>
2020-02-16 11:19:03 +0200

    format nn.rs

--- src/nn.rs
+++ src/nn.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 pub struct Neuron {
     pub bias: f64,
     pub output: f64,
-    pub weights: Vec<f64>
+    pub weights: Vec<f64>,
 }
 
 impl Neuron {
@@ -18,7 +18,11 @@ impl Neuron {
         }
         let bias = rng.gen();
         let output = 0.0;
-        Neuron { weights, bias, output }
+        Neuron {
+            weights,
+            bias,
+            output,
+        }
     }
 }
 
@@ -29,13 +33,13 @@ pub enum Activation {
     Id,
     Custom {
         activate: fn(f64) -> f64,
-        activate_prime: fn(f64) -> f64
-    }
+        activate_prime: fn(f64) -> f64,
+    },
 }
 
 pub struct Layer {
     pub neurons: Vec<Neuron>,
-    pub activation: Activation
+    pub activation: Activation,
 }
 
 impl Layer {
@@ -48,12 +52,16 @@ impl Layer {
         prev_layer_size: usize,
         rng: rand::rngs::ThreadRng,
         scale: f64,
-        activation: Activation) -> Self {
+        activation: Activation,
+    ) -> Self {
         let mut neurons = Vec::new();
         for _ in 0..size {
             neurons.push(Neuron::new_rand(prev_layer_size, rng, scale));
-        } 
-        Layer { neurons, activation }
+        }
+        Layer {
+            neurons,
+            activation,
+        }
     }
 
     fn size(&self) -> usize {
@@ -61,12 +69,12 @@ impl Layer {
     }
 
     fn get_values(&self) -> Vec<f64> {
-       self.neurons.iter().map(|n| n.output).collect()
+        self.neurons.iter().map(|n| n.output).collect()
     }
 }
 
 pub struct NeuralNetwork {
-    pub layers: Vec<Layer>
+    pub layers: Vec<Layer>,
 }
 
 impl NeuralNetwork {
@@ -105,7 +113,8 @@ impl NeuralNetwork {
                 let mut total = 0.0;
 
                 for k in 0..self.layers[i - 1].neurons.len() {
-                    total += self.layers[i - 1].neurons[k].output * self.layers[i].neurons[j].weights[k];
+                    total +=
+                        self.layers[i - 1].neurons[k].output * self.layers[i].neurons[j].weights[k];
                 }
 
                 total += self.layers[i].neurons[j].bias;
@@ -115,15 +124,23 @@ impl NeuralNetwork {
         }
         self.layers[self.layers.len() - 1].get_values()
     }
-
 }
 
 pub fn activate(x: f64, activation: Activation) -> f64 {
     match activation {
         Activation::Sigmoid => 1.0 / (1.0 + (1.0_f64).exp().powf(-x)),
-        Activation::ReLU => if x >= 0.0 { x } else { 0.0 },
+        Activation::ReLU => {
+            if x >= 0.0 {
+                x
+            } else {
+                0.0
+            }
+        }
         Activation::Id => x,
-        Activation::Custom { activate: s, activate_prime: _} => s(x)
+        Activation::Custom {
+            activate: s,
+            activate_prime: _,
+        } => s(x),
     }
 }
 
@@ -132,13 +149,21 @@ pub fn activate_prime(x: f64, activation: Activation) -> f64 {
         Activation::Sigmoid => {
             let s = activate(x, Activation::Sigmoid);
             s * (1.0 - s)
-        } 
-        Activation::ReLU => if x >= 0.0 { 1.0 } else { 0.0 },
+        }
+        Activation::ReLU => {
+            if x >= 0.0 {
+                1.0
+            } else {
+                0.0
+            }
+        }
         Activation::Id => x,
-        Activation::Custom { activate: _, activate_prime: s} => s(x)
+        Activation::Custom {
+            activate: _,
+            activate_prime: s,
+        } => s(x),
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -146,12 +171,12 @@ mod test {
 
     #[test]
     fn test_initial_layer() {
-        let nn = NeuralNetwork::new_rand(&vec![888,20,10], 1.0);
+        let nn = NeuralNetwork::new_rand(&vec![888, 20, 10], 1.0);
         assert_eq!(nn.layers[0].neurons.len(), 888);
         for i in 0..nn.layers[0].neurons.len() {
             match nn.layers[0].activation {
                 Activation::Id => (),
-                _ => panic!("non id activation in initial layer")
+                _ => panic!("non id activation in initial layer"),
             }
             assert_eq!(nn.layers[0].neurons[i].bias, 0.0);
         }
@@ -159,13 +184,13 @@ mod test {
 
     #[test]
     fn test_layer_size() {
-        let nn = NeuralNetwork::new_rand(&vec![888,20,55,3,7,10], 1.0);
+        let nn = NeuralNetwork::new_rand(&vec![888, 20, 55, 3, 7, 10], 1.0);
         assert_eq!(nn.layers.len(), 6);
     }
 
     #[test]
     fn test_neurons_length() {
-        let dim = vec![888,20,55,3,7,10];
+        let dim = vec![888, 20, 55, 3, 7, 10];
         let nn = NeuralNetwork::new_rand(&dim, 1.0);
         for i in 0..dim.len() {
             assert_eq!(nn.layers[i].neurons.len(), dim[i]);
@@ -174,23 +199,23 @@ mod test {
 
     #[test]
     fn test_weights_ranges() {
-        let dim = vec![888,20,55,3,7,10];
+        let dim = vec![888, 20, 55, 3, 7, 10];
         let nn = NeuralNetwork::new_rand(&dim, 1.0);
         for i in 0..dim.len() {
             for j in 0..nn.layers[i].neurons.len() {
                 for k in 0..nn.layers[i].neurons[j].weights.len() {
                     assert!(nn.layers[i].neurons[j].weights[k] < 1.0);
                     assert!(nn.layers[i].neurons[j].weights[k] >= 0.0);
-                } 
+                }
             }
         }
     }
 
     #[test]
     fn test_forward() {
-        let dim = vec![4,20,55,3,7,10];
+        let dim = vec![4, 20, 55, 3, 7, 10];
         let mut nn = NeuralNetwork::new_rand(&dim, 1.0);
-        let res = nn.forward(&vec![0.0,0.5,0.8,0.9]);
+        let res = nn.forward(&vec![0.0, 0.5, 0.8, 0.9]);
         assert_eq!(res.len(), 10);
     }
 }
